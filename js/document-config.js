ee25e9abd70d1e23c2e479ac9a77f4619c0505eb
gwolbers <gwolbers@users.noreply.github.com>
2025-10-22 08:05:33 +0000

    deploy: b317a2329f6eeed932bc45f4cfbe4dc1fecda0bc

---
+++ js/document-config.js
@@ -0,0 +1,27 @@
+var documentConfig =
+{
+  title: "Documentatie",
+  shortName: "DOC",
+  pubDomain: "geen",
+  specStatus: "wv",
+  specType: "bd",
+  license: "cc0",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/geen/DOC/"
+  ],
+  publishDate: "2025-10-10",
+  thisVersion: [
+    "https://geonovum.github.io/geen-DOC/"
+  ],
+  github: "https://github.com/geonovum/geen-DOC/",
+  issueBase: "https://github.com/geonovum/geen-DOC/issues/",
+  edDraftURI: "https://geonovum.github.io/geen-DOC/",
+  canonicalURI: "https://docs.geostandaarden.nl/geen/DOC/",
+  authors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}
